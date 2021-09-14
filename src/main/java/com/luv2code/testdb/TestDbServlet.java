1245e7aefca3b99dfd6ad0af626f3a3a45011a36
Ilie Babcenco <ilie.babcenco@amdaris.com>
2021-09-14 15:03:36 +0300

    Add basic configuration

--- src/main/java/com/luv2code/testdb/TestDbServlet.java
+++ src/main/java/com/luv2code/testdb/TestDbServlet.java
@@ -4,11 +4,36 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 @WebServlet(name = "TestDbServlet", value = "/TestDbServlet")
 public class TestDbServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String user = "springstudent";
+        String pass = "springstudent";
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false";
+
+        String driver = "com.mysql.jdbc.Driver";
+
+        try {
+            PrintWriter out = response.getWriter();
+            out.println("Connecting to database: " + jdbcUrl);
+            Class.forName(driver);
+
+            Connection connection = DriverManager.getConnection(jdbcUrl, user, pass);
+
+            out.println("Success!");
+
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ServletException();
+        }
 
     }
 
